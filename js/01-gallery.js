a6626fb319af66e1727b0507f92ca63127de3911
Yura Basiuk <yurik.kamenev@gmail.com>
2023-03-30 18:57:31 +0300

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+function createGalleryListMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+console.log(createGalleryListMarkup);
